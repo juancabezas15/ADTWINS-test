76488b666758a0f3e905dec8dfef6349fd9ed72c
Juan Carlos Cabezas <juan.cabezas15@inacapmail.cl>
2023-01-12 18:26:34 -0300

    main description updated

--- main.py
+++ main.py
@@ -3,9 +3,8 @@ from router.routes import router as user
 
 app = FastAPI(
     title="API REST FOR ADTWINS TEST",
-    description="API REST with FastAPI and MongoDB library Database",
+    description="### API REST with FastAPI and MongoDB library Database",
     version="1.0.0"
-
 )
 
 app.include_router(user)
